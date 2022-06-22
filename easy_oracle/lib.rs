4eb53cb96b99c55d1431a5b261f74735d1cd51d3
h4x3rotab <h4x3rotab@gmail.com>
2022-06-22 08:17:18 +0000

    make easy_oracle compatible with SubmittableOracle

--- easy_oracle/lib.rs
+++ easy_oracle/lib.rs
@@ -121,25 +121,43 @@ mod easy_oracle {
         ///
         /// It sends a HTTPS request to the url and extract an address from the claim ("This gist
         /// is owned by address: 0x..."). Once the claim is verified, it returns a signed
-        /// attestation with the pair `(github_username, account_id)`.
+        /// attestation with the data `(username, account_id)`.
+        ///
+        /// The `Err` variant of the result is an encoded `Error` to simplify cross-contract calls.
+        /// Particularly, when another contract wants to call us, they may not want to depend on
+        /// any special type defined by us (`Error` in this case). So we only return generic types.
         #[ink(message)]
-        pub fn attest_gist(&self, url: String) -> Result<attestation::Attestation> {
+        pub fn attest(
+            &self,
+            url: String,
+        ) -> core::result::Result<attestation::Attestation, Vec<u8>> {
             // Verify the URL
-            let gist_url = parse_gist_url(&url)?;
+            let gist_url = parse_gist_url(&url).map_err(|e| e.encode())?;
             // Fetch the gist content
             let resposne = http_get!(url);
             if resposne.status_code != 200 {
-                return Err(Error::RequestFailed);
+                return Err(Error::RequestFailed.encode());
             }
             let body = resposne.body;
             // Verify the claim and extract the account id
-            let account_id = extract_claim(&body)?;
+            let account_id = extract_claim(&body).map_err(|e| e.encode())?;
             let quote = GistQuote {
                 username: gist_url.username,
                 account_id,
             };
             let result = self.attestation_generator.sign(quote);
             Ok(result)
+        }
+
+        #[ink(message)]
+        pub fn admin(&self) -> AccountId {
+            self.admin.clone()
+        }
+
+        /// The attestation verifier
+        #[ink(message)]
+        pub fn verifier(&self) -> attestation::Verifier {
+            self.attestation_verifier.clone()
         }
 
         /// Helper query to return the account id of the current contract instance
@@ -312,7 +330,7 @@ mod easy_oracle {
                 mock::mock_http_request(|_| {
                     HttpResponse::ok(b"This gist is owned by address: 0x0101010101010101010101010101010101010101010101010101010101010101".to_vec())
                 });
-                let result = contract.call().attest_gist("https://gist.githubusercontent.com/h4x3rotab/0cabeb528bdaf30e4cf741e26b714e04/raw/620f958fb92baba585a77c1854d68dc986803b4e/test%2520gist".to_string());
+                let result = contract.call().attest("https://gist.githubusercontent.com/h4x3rotab/0cabeb528bdaf30e4cf741e26b714e04/raw/620f958fb92baba585a77c1854d68dc986803b4e/test%2520gist".to_string());
                 assert!(result.is_ok());
 
                 let attestation = result.unwrap();
